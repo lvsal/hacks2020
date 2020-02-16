7acecc01a95484d1f3dc2aea4e99567c24a2def2
RegmiS <sandeshreg35@gmail.com>
2020-02-16 02:14:39 -0700

    making comments stuff

--- pyscripts/static/random.js
+++ pyscripts/static/random.js
@@ -59,8 +59,45 @@
 //   }
 // }
 
-function parseThrough_Comments(){
-  
+function parseThrough_Comments(obj){
+        // Find a <table> element with id="myTable":
+      var table = document.getElementById("comments_table");
+      let current_row = 1;
+
+
+    for (var i = 0; i < obj.length; i++){
+      // Create an empty <tr> element and add it to the 1st position of the table:
+      var row = table.insertRow(current_row);
+
+      var comment_allinfo = obj[i];
+
+      let postid = comment_allinfo["postid"];
+      let coord_x = comment_allinfo["coord_x"];
+      let coord_y = comment_allinfo["coord_y"];
+      let comment = comment_allinfo["comments"];
+      let rating = comment_allinfo["rating"];
+      let username = comment_allinfo["username"];
+
+      //console.log(eventname, date, starttime, endtime, location);
+      // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+      var cell0 = row.insertCell(0);
+      var cell1 = row.insertCell(1);
+      var cell2 = row.insertCell(2);
+      var cell3 = row.insertCell(3);
+      var cell4 = row.insertCell(4);
+      //var cell5 = row.insertCell(5);
+
+      // Add some text to the new cells:
+      cell0.innerHTML = "Upvote";
+      cell1.innerHTML = rating;
+      cell2.innerHTML = comment;
+      cell3.innerHTML = username;
+      cell4.innerHTML = "Location";
+      //cell5.innerHTML = description;
+
+      current_row = current_row + 1;
+    }
+
 }
 
 function codeAddress() {
@@ -79,7 +116,7 @@ function codeAddress() {
        //console.log(stringified_json);
        var obj = JSON.parse(stringified_json);
        console.log(obj);
-       //parsingThroughUniEvents(obj);
+       parseThrough_Comments(obj)
 
   });
 }
