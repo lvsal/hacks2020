6df18445290131c595f622bbaa9c2b7a53e17a07
RegmiS <sandeshreg35@gmail.com>
2020-02-15 20:01:25 -0700

    updated the data

--- front-end/script.js
+++ front-end/script.js
@@ -120,5 +120,9 @@ function drawMap(facilities){
 }
 
 test.addFacility("Bake chef","stores", "Mac", "6166", "267", [450,682],	[450,742],	[424,682]	,[424,742], "9:00 am - 5:00 pm",	"Bake chef: viet sub");
+test.addFacility("coffee shop",	"stores",	"msc",	"2","296",	[450,753], [449,792],	[423,754],	[423,792],	"9:00 am - 5:30 pm",	"coffee house: coffee shop");
+test.addFacility("pharmacy	stores", "msc", "2", "291", [564,602], [566,673], [498,602],	[498,673], "8:30 am - 6:00 am", "pharcmy for all your pharmacy needs");
+test.addFacility("stor", "stores", "msc", "2", "278", [524,395],	[524,463],	[442,395], [442,463],	"7:00 am - 11:00 pm",	"the stor has a lot of stuff");
+test.addFacility("lse", "stores", "msc", "2", "293", [642,635],	[642,823], [574,627],	[574,826], "10:00 am - 5:00 pm", "the leadership and student engagement office");
 
 drawMap(test);
