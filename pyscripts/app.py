59fbc2b8ea0e4d34d684a849a50219654c2347a8
ravenheartss <shankar_g@icloud.com>
2020-02-16 06:09:37 -0700

    pop

--- pyscripts/app.py
+++ pyscripts/app.py
@@ -5,8 +5,8 @@ import data_manager as dm
 import Login_Create as login
 
 logged_in = ""
-err = ""
-
+err_l = ""
+err_create = ""
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
@@ -16,40 +16,36 @@ def hello():
     # lol = json.(data)
     # dumps
     if request.method == 'POST':
-        print('Incoming..')
-        print(request.get_json())  # parse as JSON
         return data, 200
 
 
 @app.route('/login_success', methods=['POST'])
 def login_success():
-    # lol = json.(data)
-    # dumps
-    global err
+
+    global err_l
     if request.method == 'POST':
-        print(err)
-        return err, 200
+        print(err_l + "    ugkjkgbkhbkj")
+        return err_l, 200
 
 
 @app.route('/create_success', methods=['POST'])
 def create_success():
-    # lol = json.(data)
-    # dumps
-    global err
-    if request.method == 'POST':
-        print(err)
-        return err, 200
 
+    global err_create
+    if request.method == 'POST':
+        print(err_create + "  cnskcnskcn")
+        return err_create, 200
 
 
 @app.route('/postmethod', methods = ['POST'])
 def get_post_javascript_data():
     global logged_in
-    global err
+    global err_l
+    err_l = ""
     jsdata = request.form['javascript_data']
     js = json.loads(jsdata)
     details = get_login(js)
-    err = details[1]
+    err_l = details[1]
     if(details[0] == 1):
         logged_in = js["user"]
         return logged_in
@@ -59,21 +55,22 @@ def get_post_javascript_data():
 
 
 @app.route('/createnew', methods = ['POST'])
-def create_Account():
-    global err
+def create_account():
+    global err_create
     global logged_in
+    err_create = ""
     jsdata = request.form['javascript_data']
     js = json.loads(jsdata)
     login.Create_User(js["email"], js["pass"], js["user"])
     hey = login.get_success()
-    err = hey[1]
-    return err
+    err_create = hey[1]
+    return err_create
 
 
 def get_login(js):
-    username = js["user"]
     login.Login(js["user"], js["pass"])
     hey = login.get_success()
+    print(hey)
     return hey
 
 
