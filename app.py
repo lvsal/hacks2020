54f88b868b71e2854ce8bfd06c140e96ce6ab5ff
ravenheartss <shankar_g@icloud.com>
2020-02-15 22:17:54 -0700

    Fixed

--- app.py
+++ app.py
@@ -1,30 +1,54 @@
 from flask import Flask, jsonify, request, render_template, make_response
-import os
-import time
-app = Flask(__name__)
 
-def format_server_time():
-  server_time = time.localtime()
-  return time.strftime("%I:%M:%S %p", server_time)
+app = Flask(__name__)
+import json
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
 
     # POST request
+    data = {
+        "president": {
+            "name": "Beeblebrox",
+            "species": "Betelgeusian"
+        }
+    }
+
+    # lol = json.(data)
+    # dumps
     if request.method == 'POST':
         print('Incoming..')
         print(request.get_json())  # parse as JSON
-        return 'OK', 200
+        return data, 200
 
     # GET request
     else:
         message = {'greeting':'Hello from Flask!'}
         return jsonify(message)  # serialize and use JSON headers
 
+@app.route('/calhack', methods=['POST'])
+def calhack():
+
+    # POST request
+    data = {
+        "president": {
+            "name": "Zaphod Beeblebrox",
+            "species": "Betelgeusian"
+        }
+    }
+
+    if request.method == 'POST':
+        print('Incoming..')
+        print(request.get_json())  # parse as JSON
+        return data, 200
+
+
+
 @app.route('/')
 def index():
     # look inside `templates` and serve `index.html`
     return render_template('index.html')
+
 
 
 if __name__ == '__main__':
